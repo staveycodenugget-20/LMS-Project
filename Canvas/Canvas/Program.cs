15bca6bea4cb0547244b7ac09189b93e4f53deef
staveycodenugget-20 <stavey.nug17@gmail.com>
2026-02-27 13:01:44 -0500

    Solutons

---
+++ Canvas/Canvas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose one of the options");
+            Console.WriteLine("1. Teacher");
+            Console.WriteLine("2. Student");
+
+            var choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Teacher Menu");
+                // You can add teacher-specific functionality here
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Student menu");
+                // You can add student-specific functionality here
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please choose either 1 or 2.");
+            }
+
+        }
+    }
+}
---
+++ LMS/CLI.LMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CLI.LMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
