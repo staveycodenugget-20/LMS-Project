7c6b59c3f3fea4a0fb27d44bdf34483dea3533f8
staveycodenugget-20 <stavey.nug17@gmail.com>
2026-03-12 14:09:30 -0400

    Models for Mock Canvas. Closes #20

---
+++ LMS/UserInformation/UserModels/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserInformation.UserModels
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Student> Roster { get; set; }
+        public List<Module> Modules { get; set; }
+        public List<Assignment> Assignments { get; set; }
+
+
+    }
+}
---
+++ LMS/UserInformation/UserModels/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserInformation.UserModels
+{
+    public class Student : User
+    {
+        public Classification Classification { get; set; }
+
+    }
+    public enum Classification
+    {
+        Unknown, Freshman, Sophomore, Junior, Senior
+    }
+}
