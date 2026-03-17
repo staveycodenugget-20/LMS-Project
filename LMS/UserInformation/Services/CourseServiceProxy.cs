6fd8f1d423846d9a1cb3ccea37335e82a1bf448c
staveycodenugget-20 <stavey.nug17@gmail.com>
2026-03-16 22:40:01 -0400

    Allows a teacher to create a list of new course. Closes #3

---
+++ LMS/UserInformation/Services/CourseServiceProxy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserInformation.UserModels;
+
+namespace UserInformation.Services
+{
+    public class CourseServiceProxy
+    {
+        private static CourseServiceProxy instance;
+        private static object objectLock = new object();
+
+        private List<Course> courses;
+        public List<Course> Courses => courses;
+        private CourseServiceProxy() { 
+            courses = new List<Course>();
+        }
+
+        public static CourseServiceProxy Current
+        {
+            get
+            {
+                lock (objectLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CourseServiceProxy();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private int LastKey => Courses.Any() ? Courses.Select(c => c.Id).Max() : 0;
+        public Course? Add(Course? course) 
+        {
+            if (course == null) 
+            {
+                return null;
+            }
+
+            if(course.Id == 0)
+            {
+                course.Id = LastKey + 1;
+            }
+            
+            courses.Add(course);
+            return course;
+        }
+    }
+}
---
+++ LMS/UserInformation/Services/StudentServiceProxy.cs
@@ -0,0 +1,53 @@
+﻿using UserInformation.UserModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLI.LMS.Services
+{
+    public class StudentServiceProxy
+    {
+        private static StudentServiceProxy? instance;
+        private static object instanceLock = new object();
+
+        public static StudentServiceProxy Current
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new StudentServiceProxy();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private List<Student> students;
+
+        public List<Student> Students => students;
+        private StudentServiceProxy() { 
+            students = new List<Student>();
+        }
+
+        public int LastKey => Students.Any() ? Students.Select(s => s.Id).Max() : 0;
+
+        public Student? Add(Student? student)
+        {
+            if(student == null)
+            {
+                return student;
+            }
+
+            if(student.Id == 0)
+            {
+                student.Id = LastKey + 1;
+            }
+
+            students.Add(student);
+            return student;
+        }
+    }
+}
