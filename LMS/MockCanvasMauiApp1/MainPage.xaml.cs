2a6f9e096419f35623ab0fbcd2e2cff2b0c66da7
staveycodenugget-20 <stavey.nug17@gmail.com>
2026-05-01 09:32:27 -0400

    Application has main menu in MAUI.NET. Closes #30

---
+++ LMS/MockCanvasMauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CLI.LMS.UserSections;
+
+namespace MockCanvasMauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTeacherClicked(object sender, EventArgs e)
+        {
+            var teacherMenu = new TeacherMenuSection();
+            teacherMenu.EnterMainMenu();
+        }
+
+        private void OnStudentClicked(object sender, EventArgs e)
+        {
+            var studentMenu = new StudentMenuSection();
+            studentMenu.EnterMainMenu();
+        }
+    }
+}
