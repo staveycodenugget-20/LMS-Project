cf20abbb98f2a0353ff6878452bf38c3a25aa3a0
staveycodenugget-20 <stavey.nug17@gmail.com>
2026-05-10 12:25:31 -0400

    Application manages student data server-side through WebAPI Controller. Closes #48

---
+++ LMS/MockCanvasAPI/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserInformation.UserModels;
+
+namespace MockCanvasAPI.Controllers
+{
+    //Test with https://localhost:7192/swagger or whatever  run program generates for local host num
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>();
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        [HttpPost]
+        public Student Post(Student student)
+        {
+            students.Add(student);
+            return student;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student != null)
+            {
+                students.Remove(student);
+            }
+        }
+    }
+}
